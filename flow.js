f2267be780c4f2001a64ac08afc14d690a4de6fd
Chad Lee <chad.lee@archoninfosys.com>
2016-10-17 09:54:53 -0500

    Fix syntax issues with new flow plugin

--- flow.js
+++ flow.js
@@ -2,26 +2,14 @@ module.exports = {
 	extends: [
 		"plugin:flowtype/recommended"
 	],
-	rules: [
-		"flowtype/require-valid-file-annotation": [
-			"error",
-			"never", {
-				annotationStyle: "block"
-			}
-		],
-		"flowtype/type-id-match": [
-			"error"
-		],
-		"flowtype/delimiter-dangle": [
-			"error",
-			"never"
-		],
-		"flowtype/no-weak-types": [
-			"error"
-		],
-		"flowtype/semi": [
-			"error",
-			"always"
-		]
-	]
+	plugins: [
+		"flowtype"
+	],
+	rules: {
+		"flowtype/require-valid-file-annotation": ["error", "never", { annotationStyle: "block" }],
+		"flowtype/type-id-match": ["error"],
+		"flowtype/delimiter-dangle": ["error", "never"],
+		"flowtype/no-weak-types": ["error"],
+		"flowtype/semi": ["error", "always"]
+	}
 };
