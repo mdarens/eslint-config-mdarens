6a3cef646153ff99a164931d0273038ee6e91ad9
Chad Lee <chad.lee@archoninfosys.com>
2017-10-19 09:22:43 -0500

    Don't include any environments by default
    Force consuming packages to specify their own environments rather than over-specifying here.
    
    +semver:major

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 module.exports = {
 	parser: "babel-eslint",
-	env: {
-		browser: true,
-		commonjs: true,
-		es6: true,
-		node: true
-	},
 	extends: [
 		"eslint:recommended",
 		"civicsource/imports",
